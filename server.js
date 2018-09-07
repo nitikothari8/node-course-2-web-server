0f4863dc818a2accbcd87b1fa35a474653a60d9c
niti <minurocks@gmail.com>
2018-09-07 18:27:46 +0530

    setup 2

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set('view engine','hbs');
 app.use((req, res, next) => {
   var d = new Date().toString();
   var log = d + " " + req.method+ " " + req.path;
-   fs.appendFile('log.txt',log + '\n');
+   //fs.appendFile('log.txt',log + '\n');
 });
 
 
